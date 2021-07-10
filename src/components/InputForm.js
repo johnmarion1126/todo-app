b1bced72debcbe3cf982afe69f27f0e5e426fc73
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-07-10 16:53:33 -0700

    Add handlers for user input and submit

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const InputForm = () => (
-  <div>
-    <button type="button">+</button>
-  </div>
-);
+const InputForm = () => {
+  const [data, setData] = useState(null);
+
+  const handleChange = (event) => {
+    setData(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Your username is: ${data}`);
+  };
+
+  return (
+    <form>
+      <input
+        type="text"
+        name="description"
+        onChange={handleChange}
+      />
+      <button
+        type="button"
+        onClick={handleSubmit}
+      >
+        x
+
+      </button>
+      <div>
+        {data}
+      </div>
+    </form>
+  );
+};
 
 export default InputForm;
