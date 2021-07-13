09a3b9c6d6058a888545d0c57ceb5b7898ca2edb
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-07-13 16:52:27 -0700

    Add date, remove test data

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import uniqid from 'uniqid';
 import InputForm from './InputForm';
 import TodoItem from './TodoItem';
-import todoData from './todosData';
 
 const App = () => {
-  const [todo, setTodo] = useState(todoData);
+  const [todo, setTodo] = useState([]);
+  let currentDate = new Date();
+  let date = currentDate.getDate();
+  let month = currentDate.getMonth() + 1;
+  let year = currentDate.getFullYear();
+
+  useEffect(() => {
+    currentDate = new Date();
+    date = currentDate.getDate();
+    month = currentDate.getMonth() + 1;
+    year = currentDate.getFullYear();
+  }, [date]);
 
   const handleChange = (id) => {
     setTodo((prevTodo) => {
@@ -38,7 +48,6 @@ const App = () => {
       });
       return updatedTodo;
     });
-    console.log(todo);
   };
 
   const todos = todo.map(
@@ -55,7 +64,14 @@ const App = () => {
 
   return (
     <div className="app-container">
-      <h1>Todo App</h1>
+      <h1>
+        Todo App -
+        {month}
+        /
+        {date}
+        /
+        {year}
+      </h1>
       {todos}
       <InputForm handleAddTask={handleAddTask} />
     </div>
